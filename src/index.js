e189295c1874c0a58a92cbac00f30421be11cc53
Amir Darzi <amir@amir2000.nl>
2026-01-19 11:33:31 +0100

    Use PORT env var for Render

--- src/index.js
+++ src/index.js
@@ -52,6 +52,8 @@ app.use(Sentry.Handlers.errorHandler());
 app.use(errorHandler);
 
 
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
